e2a5223c4971fcd147f6b87b4b4635edc09c4201
Arvicx <arthurfernandesrodrigues1@gmail.com>
2025-03-11 14:20:36 -0300

    atividade 10/03

---
+++ PHP/Atividades/atividade 10.03/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade 10/03</title>
+</head>
+<body>
+    <?php 
+        echo "<h1>Que site legal!!</h1>";
+        
+    ?>    
+    <a href="pages/cotacao.php">Cotação</a>
+    <a href="pages/idade.php">Idade</a>
+    <a href="pages/numero.php">Número Aleatório</a>
+</body>
+</html>
