1260f473b9fe8b3a2a09f59b67c62d27e5de047d
Arvicx <arthurfernandesrodrigues1@gmail.com>
2025-03-11 17:39:26 -0300

    Jogos 11/03

--- PHP/Aulas/Aula 10.03/jogosPHP/index.php
+++ PHP/Aulas/Aula 10.03/jogosPHP/index.php
@@ -6,6 +6,27 @@
     <title>Login</title>
 </head>
 <body>
+    
+    <main>
+        <form action="assets/scripts/login.php" method="post">
+            <!-- email -->
+             <div class="form-group">
+                <label for="email">Email: </label>
+                <input placeholder="Digite seu email..." type="email" required name="email" id="email">
+             </div>
+
+             <!-- senha -->
+             <div class="form-group">
+                <label for="senha">Senha: </label>
+                <input placeholder="Digite sua senha..." type="password" required name="senha" id="senha">
+             </div>
+             <button type="submit">LOGIN</button>
+             <p><a href="#">Esqueci minha senha</a></p>
+             <p><a href="assets/pages/cadastrar.php">Cadastre-se</a></p>
+
+        </form>
+
+    </main>
     <a href="assets/pages/home.php">Home</a>
 </body>
 </html>
