d2473512c20e2ee32a1f89dcd697fd8c79c1c10d
Arvicx <arthurfernandesrodrigues1@gmail.com>
2025-04-15 17:38:38 -0300

    Começo do estilo

--- PHP/Atividades/receitas/index.php
+++ PHP/Atividades/receitas/index.php
@@ -9,9 +9,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Receitas Culinária</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/index.css">
 </head>
 <body>
     <header>
+        <h1>Receitas da Didi</h1>
         <nav>
             <a href="index.php?menu=home">Home</a>
             <a href="index.php?menu=receitas">Receitas</a>
